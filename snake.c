142c4df8b6e9704640af3c00bc9a3a996ddc0d89
Leon <lelek0801@gmail.com>
2023-01-21 14:38:04 +0100

    Added border & time ticks

--- snake.c
+++ snake.c
@@ -7,28 +7,56 @@
 #define HEIGHT 23
 #define WIDTH 79
 
-int movement(int *snakeX, int *snakeY, int moveX, int moveY, int speed, WINDOW* win){
+int movement(int* snakeX, int* snakeY, int moveX, int moveY, int speed, WINDOW* win)
+{
     *snakeX = *snakeX + moveX;
     *snakeY = *snakeY + moveY;
     mvwprintw(win, *snakeY, *snakeX, "0");
     usleep(100000 * speed);
-}   
+}
 
+int kbhit(void)
+{
+    struct timeval tv;
+    fd_set read_fd;
 
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&read_fd);
+    FD_SET(0, &read_fd);
 
-int main(){
+    if (select(1, &read_fd, NULL, NULL, &tv) == -1)
+        return 0;
+
+    if (FD_ISSET(0, &read_fd))
+        return 1;
+
+    return 0;
+}
+
+int game_border(int snakeX, int snakeY)
+{
+    if (snakeX < 1 || snakeX > WIDTH-2) {
+        return 1;
+    } else if (snakeY < 1 || snakeY > HEIGHT-2) {
+        return 1;
+    }
+    return 0;
+}
+
+int main()
+{
     int snake_body[100][2];
     int score = 0;
-    int snakeX = 79;
-    int snakeY = 0;
+    int snakeX = 1;
+    int snakeY = 1;
     int action;
-    int moveX = -1;
+    int moveX = 1;
     int moveY = 0;
-    int speed = 1;
+    int speed = 3;
 
-
-    for(int i = 0; i < 100; i++){
-        for(int j = 0; j < 2; j++){
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 2; j++) {
             snake_body[i][j] = 0;
         }
     }
@@ -41,34 +69,36 @@ int main(){
 
     WINDOW* win = newwin(HEIGHT, WIDTH, 1, 1);
     keypad(win, TRUE);
-    mvprintw(0, 1, "Score: %i", score);
     box(win, 0, 0);
     wrefresh(win);
 
-    while(1){
+    while (!game_border(snakeX, snakeY)) {
         werase(win);
         box(win, 0, 0);
+        // mvprintw(0, 1, "Score: %i", score);
         movement(&snakeX, &snakeY, moveX, moveY, speed, win);
         wrefresh(win);
 
-        int action = wgetch(win);
+        if (kbhit()) {
+            int action = wgetch(win);
 
-        if (action == KEY_LEFT) {
-            moveX = -1;
-            moveY = 0;
-        } else if (action == KEY_RIGHT) {
-            moveX = 1;
-            moveY = 0;
-        } else if (action == KEY_UP) {
-            moveX = 0;
-            moveY = -1;
-        } else if (action == KEY_DOWN) {
-            moveX = 0;
-            moveY = 1;
+            if (action == KEY_LEFT) {
+                moveX = -1;
+                moveY = 0;
+            } else if (action == KEY_RIGHT) {
+                moveX = 1;
+                moveY = 0;
+            } else if (action == KEY_UP) {
+                moveX = 0;
+                moveY = -1;
+            } else if (action == KEY_DOWN) {
+                moveX = 0;
+                moveY = 1;
+            }
         }
-        
     }
 
+    mvprintw(12, 30, "game over!");
     getch();
     endwin();
 }
